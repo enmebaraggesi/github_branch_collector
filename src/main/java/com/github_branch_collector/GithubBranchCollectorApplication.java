14f804406ce158a5a02d50d12b8e8cb9566b195e
enmebaraggesi <koniec.poczatek@gmail.com>
2024-06-04 14:51:05 +0200

    Initial commit

---
+++ src/main/java/com/github_branch_collector/GithubBranchCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.github_branch_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubBranchCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubBranchCollectorApplication.class, args);
+	}
+
+}
